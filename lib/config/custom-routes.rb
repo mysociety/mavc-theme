ddc5b4bbdf59a042d9924b4b1d52ed046ccdb84d
Gareth Rees <gareth@mysociety.org>
2015-09-23 12:25:48 +0100

    fixup! Add new pages to help menu

--- lib/config/custom-routes.rb
+++ lib/config/custom-routes.rb
@@ -2,8 +2,7 @@
 # Here you can override or add to the pages in the core website
 
 Rails.application.routes.draw do
-    # brand new controller example
-    match '/mycontroller' => 'general#mycontroller'
-    # Additional help page example
-    match '/help/help_out' => 'help#help_out'
+  match '/help/foi_awareness' => 'help#foi_awareness'
+  match '/help/partners' => 'help#partners'
+  match '/help/the_iic' => 'help#the_iic'
 end

af92a2067dab92abe1822b382cfd38c1f40e8a3b
Louise Crow <louise.crow@gmail.com>
2015-11-10 16:43:49 +0000

    Remove example pages.

--- lib/controller_patches.rb
+++ lib/controller_patches.rb
@@ -6,12 +6,7 @@
 # See http://stackoverflow.com/questions/7072758/plugin-not-reloading-in-development-mode
 #
 Rails.configuration.to_prepare do
-    # Example adding an instance variable to the frontpage controller
-    GeneralController.class_eval do
-        def mycontroller
-            @say_something = "Greetings friend"
-        end
-    end
+
     HelpController.class_eval do
 
         def foi_awareness

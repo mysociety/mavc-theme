2563b9d8d5393e822300e2d4d85160ad223dff37
Graeme Porteous <graeme@rgbp.co.uk>
2020-08-11 17:16:30 +0100

    Update custom routes

--- lib/config/custom-routes.rb
+++ lib/config/custom-routes.rb
@@ -2,7 +2,15 @@
 # Here you can override or add to the pages in the core website
 
 Rails.application.routes.draw do
-  match '/help/foi_awareness' => 'help#foi_awareness'
-  match '/help/partners' => 'help#partners'
-  match '/help/the_iic' => 'help#the_iic'
+  get '/help/foi_awareness'  => 'help#foi_awareness',
+    :via => 'get',
+    :as => 'foi_awareness'
+
+  get '/help/partners'  => 'help#partners',
+    :via => 'get',
+    :as => 'partners'
+
+  get '/help/the_iic'  => 'help#the_iic',
+    :via => 'get',
+    :as => 'the_iic'
 end

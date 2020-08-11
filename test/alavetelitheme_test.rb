7bf480f8c40aaa10026e0d5aae641edb2bf74c02
Graeme Porteous <graeme@rgbp.co.uk>
2020-08-11 16:59:08 +0100

    Remove old files
    - Partials not used in core
    - File which are unused
    - Unneeded translated view
    - Test Unit example files

--- test/alavetelitheme_test.rb
+++
@@ -1,9 +0,0 @@
-# -*- encoding : utf-8 -*-
-require File.expand_path(File.join(File.dirname(__FILE__), 'test_helper'))
-class AlavetelithemeTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-
-end
--- test/test_helper.rb
+++
@@ -1,5 +0,0 @@
-# -*- encoding : utf-8 -*-
-require 'rubygems'
-require 'active_support'
-require 'minitest/autorun'
-require 'active_support/test_case'
